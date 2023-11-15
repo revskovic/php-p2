df7cf3806661b7e9db6f62f9378c67abc58036bb
revskovic <9021911@student.tcrmbo.nl>
2023-11-15 11:16:12 +0100

    opdracht 2 opnieuw

--- opdracht 1/index.php
+++ opdracht 1/index.php
@@ -1,13 +1,23 @@
-
 <?php
+//auteur: justin
+//functie: uitleg datum functie
 
-echo "<h1>Hallo wereld,</h1>";
-echo "Ik zit op het ";
-$schoolnaam = "Techniek College Rotterdam !";
-echo "$schoolnaam";
-$getal1 = "<br>5<br>";
-echo "$getal1";
-// hoe oud ben ik
-$getal2 = "17";
-echo "$getal2";
+//initialisatie
+
+
+//main
+//l voor week dag, d voor cijfer datum van de dag, F voor de maand, Y voor het jaar.
+echo "De datum is: ". date('l d F Y');
+echo "<br>";
+//z voor de dag van het jaar.
+echo "Vandaag is het de " . date("z") . "e dag van het jaar";
+echo "<br>";
+//N voor dag van de week
+echo "Wednesday is de " . date("N") . "e dag van de week";
+echo "<br>";
+//F voor de maand.
+echo "De maand " . date("F") . " heeft in totaal 30 dagen";
+echo "<br>";
+//Y voor het jaar
+echo "Het jaar " . date("Y") . " is geen schrikkeljaar";
 ?>
---
+++ opdracht 2/index.php
@@ -0,0 +1,13 @@
+
+<?php
+
+echo "<h1>Hallo wereld,</h1>";
+echo "Ik zit op het ";
+$schoolnaam = "Techniek College Rotterdam !";
+echo "$schoolnaam";
+$getal1 = "<br>5<br>";
+echo "$getal1";
+// hoe oud ben ik
+$getal2 = "17";
+echo "$getal2";
+?>
