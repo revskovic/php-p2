94646219158e33264b87c3324672b01bb696be12
revskovic <9021911@student.tcrmbo.nl>
2024-06-11 13:36:38 +0200

    opdracht 21b uitbreiding2

--- opdracht 21a/opdracht 21a.php
+++ opdracht 21a/opdracht 21a.php
@@ -27,6 +27,16 @@ class Bird extends Animal {
         echo "De vogel vliegt.<br><br>";
     }
 }
+class Parrot extends Bird {
+    public function Eat(): void {
+        echo "De vogel eet planten.<br>";
+    }
+}
+class Roofvogel extends Bird {
+    public function Eat(): void {
+        echo "De vogel eet vlees.<br>";
+    }
+}
 
 // Maak een nieuw Bird object aan genaamd "Eend"
 $eend = new Bird("Eend");
@@ -37,6 +47,20 @@ $eend->Eat();
 $eend->Sleep();
 $eend->Fly();
 
+$parkiet = new Parrot("parkiet");
+
+$parkiet->Info();
+$parkiet->Sleep();
+$parkiet->Eat();
+$parkiet->Fly();
+
+$eagle = new Roofvogel("Eagle");
+
+$eagle->Info();
+$eagle->Eat();
+$eagle->Sleep();
+$eagle->Fly();
+
 class Vis extends Animal {
     public function swim(): void {
         echo "De vis zwemt.<br>";
@@ -44,8 +68,8 @@ class Vis extends Animal {
 }
 $vis = new Vis("karper");
 
-$vis->info();
-$vis->eat();
-$vis->sleep();
-$vis->swim();
+$vis->Info();
+$vis->Eat();
+$vis->Sleep();
+$vis->Swim();
 ?>
